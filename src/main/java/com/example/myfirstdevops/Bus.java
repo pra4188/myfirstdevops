01ad048f0371c6730c18ef2039f5f2303408caa9
Prathamesh <pra4188@gmail.com>
2023-10-31 09:58:07 +0530

    Deleting Hotels Module

--- src/main/java/com/example/myfirstdevops/Bus.java
+++ src/main/java/com/example/myfirstdevops/Bus.java
@@ -2,6 +2,7 @@ package com.example.myfirstdevops;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
-public class hotels {
-    @GetMapping("/hotels")
-    public String getData() {return "Welcome to MMT .Please book your Hotels at 50% Discount" ;}
+public class Bus {
+    @GetMapping("/Bus")
+    public String getData() {return "Welcome to MMT .Please book your Travals at 50% Discount" ;}
+}
